cf8c26d022e33454b7fd2f7c5b67e5901189f797
Daniboy20 <mejiadaniel898@gmail.com>
2026-05-24 22:51:03 -0600

    Fix de Astro para Dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import mdx from "@astrojs/mdx";
-import netlify from "@astrojs/netlify";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
@@ -8,16 +7,14 @@ import { defineConfig } from "astro/config";
 import sharp from "sharp";
 import config from "./src/config/config.json";
 
-// https://astro.build/config
 export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   image: { service: sharp() },
-  output: "server",
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
+
+  output: "static",
+
   vite: { plugins: [tailwindcss()] },
   integrations: [
     react(),
